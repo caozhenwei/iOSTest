401d3e9754fc85c0ac5bff540ba99edada1fc31f
caozhenwei <296614451@qq.com>
2018-03-22 20:09:38 +0800

    init project

---
+++ iOSTest/iOSTest/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSTest
+//
+//  Created by caozhenwei on 22/03/2018.
+//  Copyright © 2018 czw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSTest/iOSTest/ViewControllers/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iOSTest
+//
+//  Created by caozhenwei on 22/03/2018.
+//  Copyright © 2018 czw. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
